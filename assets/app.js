b98ff65eafe0cccb75f945e8e57030584cba4c9b
Maud Trisson <maud.trisson@gmail.com>
2023-06-07 23:05:12 +0200

    add zoom on canvas

--- assets/app.js
+++ assets/app.js
@@ -17,7 +17,3 @@ import './bootstrap';
 import { registerReactControllerComponents } from '@symfony/ux-react';
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
 
-
-
-
-
--- assets/react/controllers/Canvas.jsx
+++ assets/react/controllers/Canvas.jsx
@@ -10,19 +10,35 @@ export default function () {
 
   const initCanvas = () => (
     new fabric.Canvas('canvas', {
-      height: 500,
+      height: 400,
       width: 900,
-      backgroundColor: 'pink'
+      backgroundColor: 'white'
     })
   )
+
   const addRect = canvi => {
+
     const rect = new fabric.Rect({
       height: 280,
       width: 200,
-      fill: 'yellow'
+      fill: 'white',
+      stroke: 'purple',
+      
     });
     canvi.add(rect);
     canvi.renderAll();
+ 
+    canvas.on('mouse:wheel', function(opt) {
+      var delta = opt.e.deltaY;
+      var zoom = canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+    });
+
   }
   return(
     <div>
